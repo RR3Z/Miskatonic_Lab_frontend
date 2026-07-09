b021531bfef0b0da501e0874a798c8387b3e091c
Chernobaev Egor <consicration@mail.ru>
2026-07-09 10:37:51 +0300

    add: russian localization for Clerk

--- src/components/auth/clerk-provider.tsx
+++ src/components/auth/clerk-provider.tsx
@@ -1,10 +1,16 @@
+import { ruRU } from "@clerk/localizations"
 import { ClerkProvider as ClerkProviderBase } from "@clerk/nextjs"
 import { ui } from "@clerk/ui"
 import { clerkAppearance } from "@/lib/clerk/appearance"
 
 export function ClerkProvider({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProviderBase appearance={clerkAppearance} prefetchUI={false} ui={ui}>
+    <ClerkProviderBase
+      appearance={clerkAppearance}
+      localization={ruRU}
+      prefetchUI={false}
+      ui={ui}
+    >
       {children}
     </ClerkProviderBase>
   )
